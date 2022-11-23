99b0771ddcabe45f2a7dd8a499f7e9c137b9363a
abhishek-parsons <113526161+abhishek-parsons@users.noreply.github.com>
2022-11-23 13:11:59 -0500

    Fix homepage issue

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import "./App.css";
 import Entry from "./components/Entry";
 import Prompt from "./components/Prompt";
-import promptList from "./prompts";
 
 function App() {
-  const randomPrompt =
-    promptList[Math.floor(Math.random() * promptList.length)];
-
   return (
     <div className="App">
       <div className="prompt-entry">
--- src/components/Prompt.js
+++ src/components/Prompt.js
@@ -1,7 +1,18 @@
+import promptList from "../prompts";
+
 function Prompt({ selected, onClick }) {
+  const randomPrompt =
+    promptList[Math.floor(Math.random() * promptList.length)];
   return (
-    <div className="prompt" onClick={onClick}>
-      Prompt
+    <div className="prompt-container" onClick={onClick}>
+      <div className="prompt">
+        <p>Date</p>
+        <p>
+          This is the prompt and here is where it will show up. It can contain
+          questions etc that the person might want to answer
+        </p>
+      </div>
+      <div className="controls">controls</div>
     </div>
   );
 }
